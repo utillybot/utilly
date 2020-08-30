f70974103bdb2018ada05352961a212ff21c7065
Justin <27078349+jtsshieh@users.noreply.github.com>
2020-08-29 20:04:44 -0400

    Update privacy policy

--- src/bot/commands/General/Privacy.ts
+++ src/bot/commands/General/Privacy.ts
@@ -20,7 +20,7 @@ export default class Privacy extends Command {
         const embed = new EmbedBuilder();
         embed.setTitle('Privacy Policy');
         embed.setDescription(
-            'By using the bot you agree to the following terms:\n* Obviously the discord TOS\n* In any server the bot is in, the **guild id** along with any settings you set (including enabled and disabled modules) will be stored in a database.'
+            'We collect the guild ids and the settings you set for that guild. For example, this would be whether a modules is enabled or not or channel ids for the log channels. The data will only be used to store your preferences and settings for you guild and not anything else. The data is stored in a database that I, jtsshieh#6424, and my hosting provider has access to. If you have any concerns or what your data removed, send me a DM and I will remove your guild settings. '
         );
         message.channel.createMessage({ embed });
     }
