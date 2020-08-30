2fa3cebabbe5b5797906c2e9c0d6c2776de63ad4
Justin <27078349+jtsshieh@users.noreply.github.com>
2020-08-29 20:10:38 -0400

    Fix privacy policy typo

--- src/bot/commands/General/Privacy.ts
+++ src/bot/commands/General/Privacy.ts
@@ -20,7 +20,7 @@ export default class Privacy extends Command {
         const embed = new EmbedBuilder();
         embed.setTitle('Privacy Policy');
         embed.setDescription(
-            'We collect the guild ids and the settings you set for that guild. For example, this would be whether a modules is enabled or not or channel ids for the log channels. The data will only be used to store your preferences and settings for you guild and not anything else. The data is stored in a database that I, jtsshieh#6424, and my hosting provider has access to. If you have any concerns or what your data removed, send me a DM and I will remove your guild settings. '
+            'We collect the guild ids and the settings you set for that guild. For example, this would be whether a modules is enabled or not or channel ids for the log channels. The data will only be used to store your preferences and settings for you guild and not anything else. The data is stored in a database that I, jtsshieh#6424, and my hosting provider have access to. If you have any concerns or want your data removed, send me a DM and I will remove your guild settings. '
         );
         message.channel.createMessage({ embed });
     }
