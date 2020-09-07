492283cc85ca40f2196b7e03edba5f25652757d0
Justin <27078349+jtsshieh@users.noreply.github.com>
2020-09-07 15:14:21 -0400

    Fix routing

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,13 +2,13 @@ import express from 'express';
 import path from 'path';
 const PORT = process.env.PORT || 3006;
 const app = express();
-const pages = ['', 'commands', 'about'];
-pages.map(page => `/${page}`);
+const pages = ['commands', 'about', ''];
 
 app.use(
     pages.map(page => `/${page}`),
     express.static(path.join(process.cwd(), 'packages', 'web', 'public'))
 );
+
 app.use(
     pages.map(page => `/${page}`),
     express.static(path.join(process.cwd(), 'packages', 'web', 'dist'))
