2e8585e36fdb7fa59a24d2694edfab78aecf2354
Justin <27078349+jtsshieh@users.noreply.github.com>
2020-11-22 15:30:39 -0500

    Part 2

--- packages/web/src/dashboard/components/Navbar/components/components/ProfileDropdown/index.tsx
+++ packages/web/src/dashboard/components/Navbar/components/components/ProfileDropdown/index.tsx
@@ -1,5 +1,31 @@
-const ProfileDropdown = (): JSX.Element => {
-    
+import Collapsable from '../../../../../../components/Collapsable/Collapsable';
+import Button from '../../../../../../components/Button';
+import styles from './index.module.scss';
+import useCollapsed from '../../../../../../components/Collapsable/CollapsableContext/useCollapsed';
+import type { RefObject } from 'react';
+
+interface ProfileDropdownProps {
+	profileRef: RefObject<HTMLDivElement>;
 }
 
-export default ProfileDropdown
+const ProfileDropdown = ({ profileRef }: ProfileDropdownProps): JSX.Element => {
+	const { setCollapsed } = useCollapsed();
+
+	return (
+		<>
+			<Collapsable
+				className={styles.dropdown}
+				style={{ width: profileRef.current?.scrollWidth + 'px' }}
+			>
+				<Button to="/dashboard/servers" onClick={() => setCollapsed(true)}>
+					My Servers
+				</Button>
+				<Button to="/dashboard/logout" onClick={() => setCollapsed(true)}>
+					Logout
+				</Button>
+			</Collapsable>
+		</>
+	);
+};
+
+export default ProfileDropdown;
